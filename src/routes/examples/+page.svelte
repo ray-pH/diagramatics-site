a1d470508e90e2df3dac41d0818332cc4f2e8edd
Rayhan Alghiffari Azizi <rayhan.azizi9@gmail.com>
2023-10-25 18:41:34 +0700

    examples: update the simple square

--- src/routes/examples/+page.svelte
+++ src/routes/examples/+page.svelte
@@ -56,9 +56,9 @@
     let big_rect   = square(150).fill('lightblue').stroke('none');
     let small_rect = square(20).fill('blue').rotate(Math.PI/4);
     let r1 = small_rect.position(big_rect.get_anchor('top-left'));
-    let r2 = small_rect.position(big_rect.get_anchor('top-right'));
-    let r3 = small_rect.position(big_rect.get_anchor('bottom-left'));
-    let r4 = small_rect.position(big_rect.get_anchor('bottom-right'));
+    let r2 = small_rect.translate(V2(75,75)).fill('white');
+    let r3 = small_rect.position(V2(-75,-75)).fill('black');
+    let r4 = small_rect.position(big_rect.parametric_point(0.25));
     draw(big_rect, r1, r2, r3, r4);
     `}
 </Diagramatics>
