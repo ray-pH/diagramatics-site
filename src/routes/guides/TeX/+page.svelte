5b7a500866fc7603b35a14ae2758cc2d9771a54d
ray-pH <rayhan.azizi9@gmail.com>
2024-04-26 14:28:33 +0700

    guides: TeX

--- src/routes/guides/TeX/+page.svelte
+++ src/routes/guides/TeX/+page.svelte
@@ -1,9 +1,10 @@
 <svelte:head>
-    <title>Shapes - Guide : Diagramatics</title>
+    <title>TeX - Guide : Diagramatics</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="{base}/fonts/ubuntu.css" rel="stylesheet">
     <link href="{base}/fonts/lm-math.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js"></script>
 </svelte:head>
 
 <script>
@@ -14,22 +15,78 @@
     import '../guides.css'
     import { base } from '$app/paths';
 
-    var width = 100;
-    var height = 100;
+    var width = 200;
+    var height = 200;
+    
+    const hidden_tex_code = `
+    let handletex = (str, conf) => {
+        return MathJax.tex2svg(str, conf).innerHTML;
+    };
+    let draw_tex = (...diagrams) => {
+        draw_to_svg_element(diagram_svg, diagram_combine(...diagrams));
+        handle_tex_in_svg(diagram_svg, handletex);
+    };
+        `
 </script>
 
 <Header />
 <div class="guides-container">
 
     <Navigation page="TeX"/>
-
-    This part of the guide is under construction.
-
+    
+    <h1>Rendering TeX expression</h1>
+    
+    <a href="https://jsitor.com/DJqa7rQPlW">(JSitor example)</a>.
+    
+    <p>
+    To make the system more flexible and to make Diagramatics not dependent on any specific TeX rendering library,
+    Diagramatics provides a way to render TeX expressions using whatever library the user wants.
+    </p>
+    
+    <p>
+    You just need to provide a function that takes a TeX string and returns the rendered SVG string.
+    </p>
+    
+    <Diagramatics title="handle_tex_in_svg" subtitle="(svg : SVGElement, texhandler : (texstr : string\, config : any) => string)" {width} height={10}>
+        {`
+        // \`svg\` is the SVG element where the TeX expressions are to be rendered.
+        // \`texhandler\` is a function that takes a TeX string and returns the rendered SVG string.
+        `}
+    </Diagramatics>
+    
+    <p>
+    One example of texhandler is using MathJax.
+    </p>
+    <p>
+    You can call the <code>handle_tex_in_svg</code> function separately, or you can call it inside the <code>draw</code> function.
+    </p>
+    
+    <Diagramatics title="draw_tex" {width} height={10}>
+        {`
+        // you can locad MathJax from CDN
+        // If you don't want to load the whole MathJax, you can load only the tex-svg.js module
+        // <script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js"><`+`/script>
+        
+        let handletex = (str, conf) => {
+            return MathJax.tex2svg(str, conf).innerHTML;
+        };
+        let draw_tex = (...diagrams) => {
+            draw_to_svg_element(diagram_svg, diagram_combine(...diagrams));
+            handle_tex_in_svg(diagram_svg, handletex);
+        };
+        `}
+    </Diagramatics>
+    
+    Diagramatics will recognize any text object in the form "$...$" (or "$$...$$" for display style) as a <b>TeX object</b>.
     <br>
     <br>
-    In the meantime, you can read this for the quick guide (<a href="https://github.com/ray-pH/diagramatics/issues/6#issuecomment-1902473874">https://github.com/ray-pH/diagramatics/issues/6#issuecomment-1902473874)</a>),
-    along with the <a href="https://jsitor.com/DJqa7rQPlW">JSitor example</a>.
-
+    
+    <Diagramatics {width} {height} hidden_code={hidden_tex_code}>
+        {`
+        let sq = square(20);
+        let tx = text('$$\\\\nabla^2\\\\phi = 0$$').fontsize(24);
+        draw_tex(sq, tx);
+        `}
+    </Diagramatics>
 
 </div>
-
